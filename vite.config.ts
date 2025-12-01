686411fda0c1594ed2fb9c2f77c2b34dca3445bf
thebrxm <75737264+thebrxm@users.noreply.github.com>
2025-12-01 15:36:35 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -17,6 +18,10 @@ export default defineConfig(({ mode }) => {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
+      export default defineConfig({
+      base: '/ECUESAPP/',
+      plugins: [react()]  
+      })
         }
       }
     };
